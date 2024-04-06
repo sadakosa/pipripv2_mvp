caa93b8323eb69460450d6ce5e98c266c7772706
sadakosa <sabrinachowzbl@gmail.com>
2024-04-06 15:02:17 +0800

    got rid of trivial comments in python files in test_api folder

--- test_apis/pdf_downloader.py
+++ test_apis/pdf_downloader.py
@@ -3,17 +3,9 @@
 import arxiv
 
 def download_pdf(paper_id):
-    # Define the base URL for the arXiv paper
     paper = next(arxiv.Client().results(arxiv.Search(id_list=[paper_id])))
-    # Download the PDF to the PWD with a default filename.
-    # paper.download_pdf()
-    # Download the PDF to the PWD with a custom filename.
-    # paper.download_pdf(filename="downloaded-paper.pdf")
-    # Download the PDF to a specified directory with a custom filename.
     paper.download_pdf(dirpath="./papers_cache", filename="downloaded-paper.pdf")
 
 if __name__ == '__main__':
     paper_id = '2404.00459'
     pdf_url = download_pdf(paper_id)
-    print(pdf_url)
-# Path: pdf_downloader.py
