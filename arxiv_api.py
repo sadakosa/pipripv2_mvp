365bef02f99dcce697743373bd250f6be7259bc1
sadakosa <sabrinachowzbl@gmail.com>
2024-04-06 14:57:24 +0800

    deleted trivial comments

--- arxiv_api.py
+++ arxiv_api.py
@@ -13,24 +13,19 @@ class ArxivDataExtractor:
     def get_paper_abstract_from_web(paper_id):
         # Define the base URL for the arXiv paper
         base_url = 'http://arxiv.org/abs/'
-        # Construct the full URL with the paper ID
         url = base_url + paper_id
-        # Send a GET request to the arXiv paper
+        
+        # Send a GET request to the arXiv paper & the abstract element within the HTML structure
         response = requests.get(url)
-        print("Response status code:", response.status_code)
-        # Parse the HTML response
         soup = BeautifulSoup(response.content, 'html.parser')
-        # Find the abstract element within the HTML structure
         abstract = soup.find('blockquote', {'class': 'abstract mathjax'})   
-        # Extract the text content of the abstract element
         abstract_text = abstract.text
-        # Return the abstract text
+        
         return abstract_text
     
     def download_pdf(paper_id):
         # Define the base URL for the arXiv paper
         paper = next(arxiv.Client().results(arxiv.Search(id_list=[paper_id])))
-        # Download the PDF to the PWD with a default filename.
         paper.download_pdf(dirpath="./papers_cache", filename=paper_id+".pdf")
     
     def extract_references_from_pdf(pdf_path):
