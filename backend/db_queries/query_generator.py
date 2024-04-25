4ff51c1bd9847c152b7387bffb30202a7efdfe06
sadakosa <sabrinachowzbl@gmail.com>
2024-04-25 23:39:30 +0800

    transferred writing papers cypher queries to the centralised cypher query generator

--- backend/db_queries/query_generator.py
+++ backend/db_queries/query_generator.py
@@ -1,39 +1,65 @@
 import json
 from backend.memgraph.db_operations import get_topics
-from backend.memgraph.database.memgraph import Memgraph
+# from backend.memgraph.database.memgraph import Memgraph
+from backend.memgraph.db_utils import cleanse
+import os
+
+class QueryGenerator:
+    def __init__(self):
+        script_directory = os.path.dirname(os.path.abspath(__file__)) # Get the directory where the script is running
+        root_directory = os.path.join(script_directory, '../..') # Define the root directory
+        os.chdir(root_directory) # Change the working directory
+        self.root_directory = root_directory
+        self.file_path = os.path.join(self.root_directory, 'backend', 'db_queries', 'dev_data.txt')
+    
+
+    # ======================== WRITING CYPHER QUERIES ========================
+
+    # take in a json string of the form in data_format.json file and produce a cypher query to insert the data into the graph with a uuid
+    def topic_cypher_query(topic, f):
+        topic_query = f"CREATE (n:Topic {{ id: '{topic.name}', description: '{topic.description}'}});\n"
+        f.write(topic_query)
+
+    def relationship_cypher_query(relationship, f):
+        f.write(f"MATCH (n1:Source {{id: '{relationship.source}'}}), (n2:Target {{id: '{relationship.target}'}}) CREATE (n1)-[:'{relationship.label}']->(n2);\n")  # edge
+
+    def paper_cypher_query(paper, f):
+        paper_query = f"CREATE (n:Paper {{ id: '{paper.title}', arxiv_id: '{paper.arxiv_id}', url: '{paper.url}', citation_count: '{paper.citation_count}', title: '{paper.title}', abstract: '{paper.abstract}', authors: '{paper.authors}', publication_date: '{paper.publication_date}', references: '{paper.references}'}});\n"
+        f.write(paper_query)  # edge
 
 
+    # take in a list of topics or relationships and produce a cypher query to insert the data into the graph with a uuid
+    def list_to_cypher_query(self, data, data_type):
+        f = open(self.file_path, "w+")
 
-# ======================== WRITING CYPHER QUERIES ========================
+        if data_type == "topic":
+            for topic in data:
+                self.topic_cypher_query(topic, f)
+        elif data_type == "relationship":
+            for relationship in data:
+                self.relationship_cypher_query(relationship, f)
+        elif data_type == "paper":
+            for paper in data:
+                self.paper_cypher_query(paper, f)
 
-# take in a json string of the form in data_format.json file and produce a cypher query to insert the data into the graph with a uuid
+        f.close()
 
-def topic_cypher_query(topic, f):
-    topic_query = f"CREATE (n:Topic {{ id: '{topic.name}', description: '{topic.description}'}});\n"
-    f.write(topic_query)
+    # Dev only: Writes out DB queries to convert the paper and references to nodes
+    # Citation edges are generated for the paper node and its reference nodes
+    def write_papers_from_api(self, paper_json):
+        print(self.file_path)
+        print("root_directory: ", self.root_directory)
+        f = open(self.file_path, "w+")
 
-def relationship_cypher_query(relationship, f):
-    f.write(f"MATCH (n1:Source {{id: '{relationship.source}'}}), (n2:Target {{id: '{relationship.target}'}}) CREATE (n1)-[:'{relationship.label}']->(n2);\n")  # edge
+        main_paper_query = f"CREATE (n:Paper {{ id: '{paper_json.paper_id}', title: '{cleanse(paper_json.title)}', name: '{cleanse(paper_json.title)}', author: {[cleanse(a) for a in paper_json.authors]}, summary: '{cleanse(paper_json.abstract)}', publication_date: '{paper_json.publication_date}'}});\n"
+        f.write(main_paper_query)
+        print("main_paper_query: ", main_paper_query)
 
-def paper_cypher_query(paper, f):
-    paper_query = f"CREATE (n:Paper {{ id: '{paper.title}', arxiv_id: '{paper.arxiv_id}', url: '{paper.url}', citation_count: '{paper.citation_count}', title: '{paper.title}', abstract: '{paper.abstract}', authors: '{paper.authors}', publication_date: '{paper.publication_date}', references: '{paper.references}'}});\n"
-    f.write(paper_query)  # edge
+        for r in paper_json.references:
+            f.write(f"CREATE (n:Paper {{ id: '{r.paper_id}', title: '{cleanse(r.title)}', name: '{cleanse(r.title)}', author: {[cleanse(a) for a in r.authors]}, summary: '{cleanse(r.abstract)}', publication_date: '{r.publication_date}'}});\n")
+            f.write(f"MATCH (n1:Paper {{id: '{r.paper_id}'}}), (n2:Paper {{id: '{paper_json.paper_id}'}}) CREATE (n1)-[:is_cited_by]->(n2);\n")  # edge
+        f.close()
 
-
-def list_to_cypher_query(data, data_type):
-    f = open("../backend/resources/gemini_data.txt", "w+")
-
-    if data_type == "topic":
-        for topic in data:
-            topic_cypher_query(topic, f)
-    elif data_type == "relationship":
-        for relationship in data:
-            relationship_cypher_query(relationship, f)
-    elif data_type == "paper":
-        for paper in data:
-            paper_cypher_query(paper, f)
-
-    f.close()
 
 
 
@@ -50,14 +76,14 @@ def is_json(myjson):
         return False
     return True
 
-def is_repeated_topic_name(topic_name):
-    db = Memgraph()
-    topics = json.loads(get_topics(db))
+# def is_repeated_topic_name(topic_name):
+#     db = Memgraph()
+#     topics = json.loads(get_topics(db))
 
-    if any(topic['name'] == topic_name for topic in topics):
-        print("The topic name is not unique")
-        return True
-    return False
+#     if any(topic['name'] == topic_name for topic in topics):
+#         print("The topic name is not unique")
+#         return True
+#     return False
 
 # ======================== MAIN FUNCTION ========================
 
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from backend.gemini.gemini_client import GeminiClient
-
+# from backend.gemini.gemini_client import GeminiClient
+from backend.paper import Paper
+from backend.api_utils.semantic_scholar_utils import search_papers_by_id
+from backend.db_queries.query_generator import QueryGenerator
 
 def main():
     papers = []
@@ -7,18 +9,22 @@ def main():
     # refs = extract_references_from_pdf("./papers_cache/2304.03442v2.pdf")
     # arxiv_ids = find_arxiv_ids_in_text(refs)
 
-    # response = search_papers_by_id(arxiv_ids=["2404.00459", "2304.03442"])
-    # for r in response:
-    #     paper = Paper(r)
-    #     paper.populateReferencesUsingSS()
-    #     papers.append(paper)
-    #     paper.save_as_json()
-    # for path in ["papers_cache/5278a8eb2ba2429d4029745caf4e661080073c81.json"]:#, "papers_cache/b9750286ba2198a406137e0dfee2d545f0d78c13.json"]:
-    #     paper = Paper.from_json(path)
-    #     print(paper)
-    #     paper.writeCypherQueries()
-    gem = GeminiClient()
-    print(gem.generate_topics_from_abstracts())
+    response = search_papers_by_id(arxiv_ids=["2404.00459", "2304.03442"])
+    for r in response:
+        paper = Paper(r)
+        paper.populateReferencesUsingSS()
+        papers.append(paper)
+        paper.save_as_json()
+    for path in ["papers_cache/5278a8eb2ba2429d4029745caf4e661080073c81.json"]:#, "papers_cache/b9750286ba2198a406137e0dfee2d545f0d78c13.json"]:
+        paper = Paper.from_json(path)
+        print(paper)
+        query_generator = QueryGenerator()
+        query_generator.write_papers_from_api(paper)
+    
+    print(papers)
+    # Gemini
+    # gem = GeminiClient()
+    # print(gem.generate_topics_from_abstracts())
 
     '''
     Gemini output
