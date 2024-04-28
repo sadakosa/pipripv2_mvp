c6d4688ecfaaff96d29b623298f744b3864533d5
sadakosa <sabrinachowzbl@gmail.com>
2024-04-28 15:20:59 +0800

    drag ended update to the latest versions to deal with an error

--- static/js/modified_index_vF.js
+++ static/js/modified_index_vF.js
@@ -253,9 +253,13 @@ import { D3TopicNode, D3PaperNode, D3Link } from './d3_models.js';
 
     function dragended(d) {
         if (!d3.event.active) simulation.alphaTarget(0);
-        event.subject.fixed = true;
+        
         d.fx = null;
         d.fy = null;
+        
+        if (!d.fixed) { 
+            d.fixed = true; 
+        }
     }
 
     window.addEventListener("resize", function() {
